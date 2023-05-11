6690147b817c1587e1a197a6c47dc9c8fb5590f0
Varun Suresh <varun.suresh@mongodb.com>
2023-05-11 11:44:08 +0530

    Modified scripts as per Cisco's requirements

---
+++ aggregateCountVerifier.js
@@ -0,0 +1,149 @@
+aggregateCountVerifier = function (loggingDbName,
+    destUsername,
+    destPassword,
+    destConnString,
+    collectionWhiteList) {
+
+    //initialise databaseWhitelist
+    if (collectionWhiteList && !Array.isArray(collectionWhiteList)) {
+        print("*** Collection whitelist: " + JSON.stringify(collectionWhiteList));
+        print("collectionWhiteList should be an array or null if no white list is required");
+        return;
+    }
+    else if (collectionWhiteList && Array.isArray(collectionWhiteList)) {
+        if (collectionWhiteList.length === 0) {
+            print("*** Collection whitelist: " + JSON.stringify(collectionWhiteList));
+            print("collectionWhiteList is empty array, but should contain at least one collection to verify");
+            return;
+        }
+        else {
+            print("*** collectionWhiteList whitelist: " + JSON.stringify(collectionWhiteList));
+        }
+    }
+    else {
+        print("*** No collectionWhiteList whitelist provided. Proceeding with all collections.");
+    }
+
+    loggingDb = db.getSiblingDB(loggingDbName);
+    jobCollName = "aggcountjob";
+    logCollName = "aggcountlog";
+
+    runId = ObjectId();
+    print("*** Starting new run with id " + runId.toString());
+    loggingDb.getCollection(jobCollName).insert({ _id: runId, verificationType: "aggCount", start: ISODate() });
+    runSummary = {
+        collNames: {
+            processed: [],
+            skipped: []
+        },
+        collDetails: {
+            processed: 0,
+            skipped: 0,
+            matches: 0,
+            mismatches: 0
+        }
+    }
+
+    destMongo = new Mongo(destConnString).getDB("admin")
+    if (!destPassword) {
+        print("Executing passwordPrompt() function to obtain destination password")
+        pwd = passwordPrompt();
+    }
+    else if (destPassword && typeof destPassword === 'function') {
+        print("Executing supplied password function to obtain destination password")
+        pwd = destPassword();
+    }
+    else {
+        pwd = destPassword;
+    }
+    destMongo.auth(destUsername, pwd);
+    destLoggingDB = destMongo.getSiblingDB(loggingDbName);
+
+    collectionWhiteList.forEach(countChecker);
+
+    function countChecker(dbNamespace) {
+        dbNameCollNameArray = dbNamespace.split('.');
+
+        sourceDB = db.getSiblingDB(dbNameCollNameArray[0]);
+        destDB = destLoggingDB.getSiblingDB(dbNameCollNameArray[0]);
+
+        if (dbNameCollNameArray[0] !== 'admin' && dbNameCollNameArray[0] != 'local' && dbNameCollNameArray[0] != 'config') {
+
+            print("Processing collection: " + dbNamespace);
+            collStartDate = ISODate();
+            addUniqueValueToArray(dbNamespace, runSummary.collNames.processed);
+            runSummary.collDetails.processed++;
+
+            sourceColl = sourceDB.getSiblingDB(dbNameCollNameArray[0]).getCollection(dbNameCollNameArray[1]);
+            destColl = destDB.getSiblingDB(dbNameCollNameArray[0]).getCollection(dbNameCollNameArray[1]);
+
+            //load counts to work with
+            sourceCollCount = sourceColl.aggregate([
+                { $group: { _id: null, count: { $sum: 1 } } },
+                { $project: { _id: 0 } }
+            ]).toArray()[0].count;
+
+            destCollCount = destColl.aggregate([
+                { $group: { _id: null, count: { $sum: 1 } } },
+                { $project: { _id: 0 } }
+            ]).toArray()[0].count;
+
+            if (sourceCollCount === destCollCount) {
+                runSummary.collDetails.matches++;
+            }
+
+            else {
+                runSummary.collDetails.mismatches++;
+            }
+
+            resultDoc = {
+                runId: runId,
+                ns: dbNamespace,
+                skipped: false,
+                verificationType: "aggCount",
+                start: collStartDate,
+                end: ISODate(),
+                srcCount: sourceCollCount,
+                dstCount: destCollCount,
+                matched: (sourceCollCount === destCollCount)
+            }
+
+            loggingDb.getCollection(logCollName).insert(resultDoc);
+        }
+    }
+
+    runSummary.end = ISODate();
+    loggingDb.getCollection(jobCollName).update({ _id: runId }, { $set: runSummary });
+
+    print("*** Aggregate Count Results Summary ...")
+    job = loggingDb.getCollection(jobCollName).findOne({ _id: runId });
+    print(JSON.stringify(job, null, '\t'))
+    print("*** Mismatches ...")
+    results = loggingDb.getCollection(logCollName).find({ runId: runId, matched: false }).toArray();
+    if (results && results.length > 0) {
+        print("*** To view the _id of all the mismatched collections, run the following on the source database ...");
+        print("   use " + loggingDbName);
+        print("   db." + logCollName + ".find({runId: " + runId.toString() + ", matched: false}).pretty()");
+        print("*** ")
+    }
+    print(JSON.stringify(results, null, '\t'))
+
+}
+
+
+addUniqueValueToArray = function (value, array) {
+    if (array.indexOf(value) < 0) {
+        array.push(value);
+    }
+}
+
+aggregateCountVerifier(
+    "test", //loggingDbName
+    "myAtlasDBUser", //destUsername
+    "MongoDB123", //destPassword - can be <string>, <function reference> e.g. passwordPrompt, null will execute passwordPrompt()
+    "mongodb+srv://cluster1.nnjyy.mongodb.net", //destination connection string (ignores credentials)
+    ["sample_training.inspections", "sample_training.zips", "sample_mflix.movies"] //collectionWhitelist
+);
+
+
+
--- countVerifier.js
+++ countVerifier.js
@@ -1,27 +1,27 @@
 countVerifier = function (loggingDbName,
-                          destUsername,
-                          destPassword,
-                          destConnString, 
-                          databaseWhitelist){
+  destUsername,
+  destPassword,
+  destConnString,
+  collectionWhiteList) {
 
   //initialise databaseWhitelist
-  if (databaseWhitelist && !Array.isArray(databaseWhitelist)){
-    print("*** Database whitelist: " + JSON.stringify(databaseWhitelist));
-    print("databaseWhitelist should be an array or null if no white list is required");
+  if (collectionWhiteList && !Array.isArray(collectionWhiteList)) {
+    print("*** Collection whitelist: " + JSON.stringify(collectionWhiteList));
+    print("collectionWhiteList should be an array or null if no white list is required");
     return;
   }
-  else if (databaseWhitelist && Array.isArray(databaseWhitelist)){
-    if (databaseWhitelist.length === 0){
-      print("*** Database whitelist: " + JSON.stringify(databaseWhitelist));
-      print("databaseWhitelist is empty array, but should contain at least one database to verify. If all databases should be verified, pass null for databaseWhitelist.");
+  else if (collectionWhiteList && Array.isArray(collectionWhiteList)) {
+    if (collectionWhiteList.length === 0) {
+      print("*** Collection whitelist: " + JSON.stringify(collectionWhiteList));
+      print("collectionWhiteList is empty array, but should contain at least one collection to verify");
       return;
     }
     else {
-      print("*** Database whitelist: " + JSON.stringify(databaseWhitelist));
+      print("*** collectionWhiteList whitelist: " + JSON.stringify(collectionWhiteList));
     }
   }
   else {
-    print("*** No database whitelist provided. Proceeding with all databases.");
+    print("*** No collectionWhiteList whitelist provided. Proceeding with all collections.");
   }
 
   loggingDb = db.getSiblingDB(loggingDbName);
@@ -30,143 +30,112 @@ countVerifier = function (loggingDbName,
 
   runId = ObjectId();
   print("*** Starting new run with id " + runId.toString());
-  loggingDb.getCollection(jobCollName).insert({_id: runId, verificationType: "count", start: ISODate()});
+  loggingDb.getCollection(jobCollName).insert({ _id: runId, verificationType: "count", start: ISODate() });
   runSummary = {
-    db : {
-      processed : [],
+    collNames: {
+      processed: [],
       skipped: []
     },
-    coll : {
-      processed : 0,
-      skipped : 0,
+    collDetails: {
+      processed: 0,
+      skipped: 0,
       matches: 0,
-      mismatches: 0 
+      mismatches: 0
     }
   }
-  
+
   destMongo = new Mongo(destConnString).getDB("admin")
-  if (!destPassword){
+  if (!destPassword) {
     print("Executing passwordPrompt() function to obtain destination password")
     pwd = passwordPrompt();
   }
-  else if (destPassword && typeof destPassword === 'function'){
+  else if (destPassword && typeof destPassword === 'function') {
     print("Executing supplied password function to obtain destination password")
     pwd = destPassword();
   }
-  else{
+  else {
     pwd = destPassword;
   }
   destMongo.auth(destUsername, pwd);
   destLoggingDB = destMongo.getSiblingDB(loggingDbName);
 
-  db.adminCommand("listDatabases").databases.forEach(function(d) {
-    sourceDB = db.getSiblingDB(d.name);
-    destDB = destLoggingDB.getSiblingDB(d.name);
+  collectionWhiteList.forEach(countChecker);
 
-    if (d.name !== 'admin' && d.name != 'local' && d.name != 'config') {
-      sourceDB.getCollectionInfos().forEach(function(c) {
-        collStartDate = ISODate();
-        ns = d.name + "." + c.name;
+  function countChecker(dbNamespace) {
+    dbNameCollNameArray = dbNamespace.split('.');
 
-        if (d.name === loggingDbName){
-          print("Skipping loggingDb collection: " + ns);
-          addUniqueValueToArray(d.name, runSummary.db.skipped);
-          recordSkippedCollection(loggingDb, logCollName, runSummary, ns, runId, collStartDate);
-        }
-        else if (c.name.startsWith('system.')){
-          print("Skipping system collection: " + ns);
-          recordSkippedCollection(loggingDb, logCollName, runSummary, ns, runId, collStartDate);
-        }
-        else if (databaseWhitelist && Array.isArray(databaseWhitelist) && databaseWhitelist.indexOf(d.name) < 0){
-          print("Skipping collection as not part of database whitelist: " + ns);
-          addUniqueValueToArray(d.name, runSummary.db.skipped);
-          recordSkippedCollection(loggingDb, logCollName, runSummary, ns, runId, collStartDate);
-        }
-        else {
-          print("Processing collection: " + ns);
-          addUniqueValueToArray(d.name, runSummary.db.processed);
-          runSummary.coll.processed++;
+    sourceDB = db.getSiblingDB(dbNameCollNameArray[0]);
+    destDB = destLoggingDB.getSiblingDB(dbNameCollNameArray[0]);
 
-          sourceColl = sourceDB.getSiblingDB(d.name).getCollection(c.name);
-          destColl = destDB.getSiblingDB(d.name).getCollection(c.name);
-          
-          //load counts to work with
-          sourceCollCount = sourceColl.count();
-          destCollCount = destColl.count();
-          if (sourceCollCount === destCollCount){
-            runSummary.coll.matches++;
-          }
-          else{
-            runSummary.coll.mismatches++;
-          }
+    if (dbNameCollNameArray[0] !== 'admin' && dbNameCollNameArray[0] != 'local' && dbNameCollNameArray[0] != 'config') {
 
-          resultDoc = {
-            runId : runId,
-            ns : ns,
-            skipped: false,
-            verificationType: "count",
-            start : collStartDate,
-            end : ISODate(),
-            srcCount : sourceCollCount, 
-            dstCount : destCollCount,
-            matched : (sourceCollCount === destCollCount)
-          }
+      print("Processing collection: " + dbNamespace);
+      collStartDate = ISODate();
+      addUniqueValueToArray(dbNamespace, runSummary.collNames.processed);
+      runSummary.collDetails.processed++;
 
-          loggingDb.getCollection(logCollName).insert(resultDoc);
-        }
-      });
+      sourceColl = sourceDB.getSiblingDB(dbNameCollNameArray[0]).getCollection(dbNameCollNameArray[1]);
+      destColl = destDB.getSiblingDB(dbNameCollNameArray[0]).getCollection(dbNameCollNameArray[1]);
+
+      //load counts to work with
+      sourceCollCount = sourceColl.count();
+      destCollCount = destColl.count();
+
+      if (sourceCollCount === destCollCount) {
+        runSummary.collDetails.matches++;
+      }
+
+      else {
+        runSummary.collDetails.mismatches++;
+      }
+
+      resultDoc = {
+        runId: runId,
+        ns: dbNamespace,
+        skipped: false,
+        verificationType: "count",
+        start: collStartDate,
+        end: ISODate(),
+        srcCount: sourceCollCount,
+        dstCount: destCollCount,
+        matched: (sourceCollCount === destCollCount)
+      }
+
+      loggingDb.getCollection(logCollName).insert(resultDoc);
     }
-  });
-  
+  }
+
   runSummary.end = ISODate();
-  loggingDb.getCollection(jobCollName).update({_id: runId}, {$set: runSummary});
-  
+  loggingDb.getCollection(jobCollName).update({ _id: runId }, { $set: runSummary });
+
   print("*** Results Summary ...")
-  job = loggingDb.getCollection(jobCollName).findOne({_id: runId});
-  print(JSON.stringify(job,null,'\t'))
-  print("*** Skipped collections  ...")
-  results = loggingDb.getCollection(logCollName)
-                      .find({runId: runId, skipped: true}, {_id:0, ns:1})
-                      .toArray().map(function(x){return x.ns;});
-  print(JSON.stringify(results,null,'\t'));
-  print("*** To show details of all the processed collections, run the following on the source database ...");
-  print("   use " + loggingDbName);
-  print("   db." + logCollName + ".find({runId: " + runId.toString() + ", skipped: false}).pretty()");
+  job = loggingDb.getCollection(jobCollName).findOne({ _id: runId });
+  print(JSON.stringify(job, null, '\t'))
   print("*** Mismatches ...")
-  results = loggingDb.getCollection(logCollName).find({runId: runId, matched: false}).toArray();
-  if (results && results.length > 0){
+  results = loggingDb.getCollection(logCollName).find({ runId: runId, matched: false }).toArray();
+  if (results && results.length > 0) {
     print("*** To view the _id of all the mismatched collections, run the following on the source database ...");
     print("   use " + loggingDbName);
     print("   db." + logCollName + ".find({runId: " + runId.toString() + ", matched: false}).pretty()");
     print("*** ")
   }
-  print(JSON.stringify(results,null,'\t'))
+  print(JSON.stringify(results, null, '\t'))
 
 }
 
-addUniqueValueToArray = function (value, array){
-  if (array.indexOf(value) < 0){
+addUniqueValueToArray = function (value, array) {
+  if (array.indexOf(value) < 0) {
     array.push(value);
   }
 }
 
-recordSkippedCollection = function(loggingDb, logCollName, runSummary, ns, runId, collStartDate){
-  runSummary.coll.skipped++;
-  resultDoc = {
-            runId : runId,
-            ns : ns,
-            skipped: true,
-            verificationType: "count",
-            start : collStartDate,
-            end : collStartDate,
-          }
-  loggingDb.getCollection(logCollName).insert(resultDoc);
-}
-
 countVerifier(
-  "local", //loggingDbName
-  "admin", //destUsername
-  null, //destPassword - can be <string>, <function reference> e.g. passwordPrompt, null will execute passwordPrompt()
-  "", //destination connection string (ignores credentials)
-  [""] //databaseWhitelist (optional) - provide null or do not supply parameter to process all dbs
+  "test", //loggingDbName
+  "myAtlasDBUser", //destUsername
+  "MongoDB123", //destPassword - can be <string>, <function reference> e.g. passwordPrompt, null will execute passwordPrompt()
+  "mongodb+srv://cluster1.nnjyy.mongodb.net", //destination connection string (ignores credentials)
+  ["sample_training.grades", "sample_training.inspections", "sample_training.posts", "sample_training.routes", "sample_training.trips", "sample_training.zips", "sample_mflix.movies"] //collectionWhitelist
 );
+
+
+
